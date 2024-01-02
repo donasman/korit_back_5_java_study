b1a6e4dda6d8ce5eaa962090caea24ac12c9b0a6
code1218 <skjil1218@gmail.com>
2024-01-02 16:14:48 +0900

    init: 수업자료 최초 커밋

---
+++ src/main/java/com/study/ch02/PersonMain.java
@@ -0,0 +1,25 @@
+package com.study.ch02;
+
+public class PersonMain {
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.name = "김준일";
+        p.age = 30;
+
+        Person p2 = new Person();
+        p2.name = "김준이";
+        p2.age = 31;
+
+        int a = 10;
+
+        /*
+        * 이름(name), 학년(studentYeal), 주소(address)를
+        * 저장할 수 있는 StudentType 클래스를 만드시오.
+        * 이름: 김준일
+        * 학년: 1학년
+        * 주소: 부산 동래구
+        * */
+    }
+
+}
---
+++ src/main/java/com/study/ch04/Operation06.java
@@ -0,0 +1,27 @@
+package com.study.ch04;
+
+public class Operation06 {
+
+    public static void main(String[] args) {
+        /*
+        * << 문제 >>
+        *
+        * 조건연산자(삼항연산자)
+        *
+        * a > 0 ? "양수" : a == 0 ? "0" : "음수"
+        *
+        *
+        * */
+
+        int iResult = 10 > 2 ? 1111 : 2222;
+
+        String sResult = 10 > 2 ? "1111" + 1 : "2222";
+        Boolean bResult = 10 > 2;
+
+        Boolean.parseBoolean("true");
+
+        String name = null;
+        System.out.println(name == null);
+    }
+
+}
