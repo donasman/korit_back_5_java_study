dd59a11e7559fe24f87fb46c26c6e23e61f41999
code1218 <skjil1218@gmail.com>
2024-01-04 17:56:00 +0900

    별찍기, 반복문 break, continue, 클래스, 객체, 인스턴스, 생성자, 생성자 오버로드

--- src/main/java/com/study/ch06/For03.java
+++ src/main/java/com/study/ch06/For03.java
@@ -15,6 +15,58 @@ public class For03 {
             System.out.println();
         }
 
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        /*
+
+          *
+         **
+        ***
+
+         */
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < starCount - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (i + 1) * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         /*
 
         *
